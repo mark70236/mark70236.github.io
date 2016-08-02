2810dbb99e62290773bdcfc941e74ec700247f92
mark70236 <mark70236@gmail.com>
2016-08-02 17:59:21 +0800

    php test
    php test

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="zh-tw">
+<head>
+	<meta charset="UTF-8">
+	<title>~精靈國の魔鴿使い~</title>
+	<meta property="og:image" content="//image.funmily.com/klee/open/logo.png">
+	<link type="image/x-icon" rel="shortcut icon" href="//image.funmily.com/klee/favicon.ico">
+	<link rel="stylesheet" href="css/reset.min.css">
+	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="css/index.css">
+	<script src="js/1.9.1.min.js"></script>
+	<script src="js/min.js"></script>
+	<script src="js/main.js"></script>
+	<script src="js/index.js"></script>
+	<script>
+		$(function() {
+			new WOW().init();
+		});
+	</script>
+</head>
+	<body class="wow fadeIn" data-wow-duration="1s" data-wow-delay="0s">
+		<? require ("wrap.php") ?>
+	</body>
+</html>
---
+++ wrap.php
@@ -0,0 +1,23 @@
+<div class="bg"></div>
+<div class="wrap">
+	<div class="border">
+		<div class="lightball"></div>
+		<div class="circle-box wow zoomIn" data-wow-duration="0.6s" data-wow-delay="3.3s">
+			<div class="circle-outside"></div>
+			<div class="circle-inside"></div>
+			<div class="circle-light"></div>
+		</div>
+		<div class="chr-box">
+
+			<div class="angel-box wow bounceIn" data-wow-duration="1s" data-wow-delay="4.5s"><div class="angel"></div></div>
+			<div class="cat-box wow bounceIn" data-wow-duration="1s" data-wow-delay="4.3s"><div class="cat"></div></div>
+			<div class="spider-box wow fadeInRight" data-wow-duration="1s" data-wow-delay="4.1s"><div class="spider"></div></div>
+			<div class="princess-box wow fadeInLeft" data-wow-duration="0.7s" data-wow-delay="3.8s"><div class="princess"></div></div>
+		</div>
+		<div class="logo"></div>
+		<div class="start-box wow fadeInDown" data-wow-duration="1s" data-wow-delay="4.6s">
+			<div class="start"></div>
+		</div>
+		<div class="copyright"></div>
+	</div>
+</div>
