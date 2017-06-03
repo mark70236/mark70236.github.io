ee95e74a5cd15ff25acc64ef1c2de4e9d8a41f2b
mark70236 <mark70236@gmail.com>
2017-06-03 11:50:17 +0800

    變更主頁
    變更主頁-作品集

---
+++ js/fit.js
@@ -0,0 +1,53 @@
+$(function() {
+
+    $(".project").mouseover(function() {
+        $(".project").removeClass('hover')
+        $(this).children(".text").addClass('close')
+        $(this).addClass('hover')
+
+    });
+    $(".project").mouseleave(function() {
+        $(this).removeClass('hover')
+        $(".text").removeClass('close')
+    });
+
+    TweenMax.set($('.project-list .project'), {
+        rotationY: 0,
+        rotationX: 0,
+        rotationZ: 0,
+        transformPerspective: 1000
+    });
+    $('.project-list .project, .project-list2 .project').mouseover(function() {
+        $('.project-list .project, .project-list2 .project').mousemove(function(e) {
+            var x = e.pageX - $(this).offset().left
+              , y = e.pageY - $(this).offset().top;
+
+            var px = x / $(this).width()
+              , py = y / $(this).height();
+            var xx = -30 + (60 * px)
+              , yy = 30 - (60 * py);
+
+            //TweenMax.killTweensOf($(this));
+            TweenMax.to($(this), 0.7, {
+                rotationY: xx,
+                rotationX: yy,
+                rotationZ: 0,
+                transformPerspective: 1000,
+                ease: Quad.easeOut
+            });
+        });
+    }).mouseout(function() {
+        $(this).unbind('mousemove');
+        //TweenMax.killTweensOf($(this));
+        TweenMax.to($(this), 0.7, {
+            rotationY: 0,
+            rotationX: 0,
+            rotationZ: 0,
+            transformPerspective: 1000,
+            ease: Quad.easeOut
+        });
+    });
+
+
+})
+
