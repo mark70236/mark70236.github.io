78cdd4c670bff373bb7cc021c00025b21ea3c16b
mark70236 <mark70236@gmail.com>
2017-06-04 01:35:06 +0800

    UPDATE

--- resourse/js/index.js
+++ resourse/js/index.js
@@ -2,10 +2,12 @@ $(function() {
 	$(".project").mouseover(function() {
 		$(".project").removeClass('hover')
 		$(this).children().children(".text").addClass('close')
+		$(this).children().children("img").addClass('move')
 		$(this).addClass('hover')
 	});
 	$(".project").mouseleave(function() {
 		$(this).removeClass('hover')
 		$(".text").removeClass('close')
+		$(this).children().children("img").removeClass('move')
 	});
 })
