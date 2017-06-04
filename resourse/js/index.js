9ab7de1e84197f9689afc4aad4734a2c1d76b034
mark70236 <mark70236@gmail.com>
2017-06-04 21:38:28 +0800

    update

--- resourse/js/index.js
+++ resourse/js/index.js
@@ -1,4 +1,5 @@
 $(function() {
+	goTop()
 	$(".project").mouseover(function() {
 		$(".project").removeClass('hover')
 		$(this).children().children(".text").addClass('close')
@@ -11,3 +12,7 @@ $(function() {
 		$(this).children().children("img").removeClass('move')
 	});
 })
+function goTop () {
+	$(window).scroll(function() { if ( $(this).scrollTop() > 600 ) { $(".gotop").css("opacity",1) } else { $(".gotop").css("opacity",0) } })
+	$(".gotop").click(function() { $("html, body").animate({ scrollTop : 0 }, 800,"easeInOutQuart") })
+}
