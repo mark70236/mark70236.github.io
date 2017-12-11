a473c9f202bf612238be69537ec8a58e25c3ea09
mark70236 <mark70236@gmail.com>
2017-12-11 14:40:07 +0800

    update

--- difference-comparison/js/index.js
+++ difference-comparison/js/index.js
@@ -1,7 +1,9 @@
 $(function() {
-	var containerWidth = $(".comparison-box").width();
-	var containerHeightString = $(".comparison-box").height()+"px";
+	var container = $(".comparison-box")
+	var containerWidth = container.width();
+	var containerHeightString = container.height()+"px";
 	var canMove = 0;
+
 	$(".scrollbar").mousedown(function(e) {
 		canMove = 1;
 		var firstX = (e.pageX - $(".wrap").offset().left)/containerWidth*100+"%";
@@ -13,6 +15,13 @@ $(function() {
 			if (canMove==1) {
 				var moveX = (e.pageX - $(".wrap").offset().left)/containerWidth*100+"%";
 				var moveChangeX = e.pageX - $(".wrap").offset().left+"px";
+				if(e.pageX - $(".wrap").offset().left<0) {
+					moveX = 0+"%";
+					moveChangeX = 0+"px";
+				}else if (e.pageX - $(".wrap").offset().left>containerWidth) {
+					moveX = 100+"%";
+					moveChangeX = containerWidth+"px";
+				}
 				$(this).css('left', moveX);
 				$(".image-box-2").css('clip', "rect(0,"+moveChangeX+","+containerHeightString+",0)");
 				console.log(moveX)
