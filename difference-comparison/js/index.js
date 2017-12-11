510f367877a6d50e815a1f1db8e895f971e8c931
mark70236 <mark70236@gmail.com>
2017-12-11 15:23:50 +0800

    update mobile

--- difference-comparison/js/index.js
+++ difference-comparison/js/index.js
@@ -4,6 +4,7 @@ $(function() {
 	var containerHeightString = container.height()+"px";
 	var canMove = 0;
 
+	//按下左鍵
 	$(".scrollbar").mousedown(function(e) {
 		canMove = 1;
 		var firstX = (e.pageX - $(".wrap").offset().left)/containerWidth*100+"%";
@@ -27,10 +28,36 @@ $(function() {
 				console.log(moveX)
 			}
 		});
+
+	});
+	$(".scrollbar").on('touchmove',function(e){
+		//console.log("123")
+		canMove = 1;
+		if (canMove==1) {
+			var moveX = (e.originalEvent.touches[0].pageX - $(".wrap").offset().left)/containerWidth*100+"%";
+			var moveChangeX = e.originalEvent.touches[0].pageX - $(".wrap").offset().left+"px";
+
+			if(e.originalEvent.touches[0].pageX - $(".wrap").offset().left<0) {
+				moveX = 0+"%";
+				moveChangeX = 0+"px";
+			}else if (e.originalEvent.touches[0].pageX - $(".wrap").offset().left>containerWidth) {
+				moveX = 100+"%";
+				moveChangeX = containerWidth+"px";
+			}
+			$(this).css('left', moveX);
+			$(".image-box-2").css('clip', "rect(0,"+moveChangeX+","+containerHeightString+",0)");
+
+		}
 	});
 
+	//放開左鍵
 	$(".scrollbar").mouseup(function(e) {
 		canMove = 0;
 		console.log("up")
+	});
+
+	//移出瀏覽器
+	$(document).mouseleave(function() {
+		canMove = 0;
 	});
 })
