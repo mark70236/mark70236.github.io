f5677c02b4eb0f3ea05d9795a08b1e6cbe572796
mark70236 <mark70236@gmail.com>
2017-12-11 14:25:27 +0800

    update

--- difference-comparison/js/index.js
+++ difference-comparison/js/index.js
@@ -19,22 +19,9 @@ $(function() {
 			}
 		});
 	});
-		$(".comparison-box").mousemove(function(e) {
-			if (canMove==1) {
-				e = $(this).find('.scrollbar')
-				var moveX = (e.pageX - $(".wrap").offset().left)/containerWidth*100+"%";
-				var moveChangeX = e.pageX - $(".wrap").offset().left+"px";
-				$(this).css('left', moveX);
-				$(".image-box-2").css('clip', "rect(0,"+moveChangeX+","+containerHeightString+",0)");
-				console.log(moveX)
-			}
-		});
 
 	$(".scrollbar").mouseup(function(e) {
 		canMove = 0;
 		console.log("up")
-	});
-	$(".comparison-box").mouseover(function(e) {
-		canMove = 0;
 	});
 })
