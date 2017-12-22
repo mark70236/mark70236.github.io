303bab0eeb853dabef9957850812cab36e479db2
mark70236 <mark70236@gmail.com>
2017-12-22 14:22:26 +0800

    update

--- project-snow/js/index.js
+++ project-snow/js/index.js
@@ -30,12 +30,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	window.addEventListener("orientationchange",onOrientationchange ,false);
 	function onOrientationchange() {
 		if (window.orientation === 180 || window.orientation === 0) {
-			reStart();
-			reStart();
+			document.querySelector('.test-box').click();
 		}
 		if (window.orientation === 90 || window.orientation === -90 ){
-			reStart();
-			reStart();
+			document.querySelector('.test-box').click();
 		}
 	}
 
