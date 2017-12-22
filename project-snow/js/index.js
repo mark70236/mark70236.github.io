2a7ab472d01e4f0f9e51cc6b5e2296bb8cd9ecb2
mark70236 <mark70236@gmail.com>
2017-12-22 13:54:08 +0800

    project-snow update

--- project-snow/js/index.js
+++ project-snow/js/index.js
@@ -15,20 +15,34 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	var windowHalfX = window.innerWidth / 2;
 	var windowHalfY = window.innerHeight / 2;
 
+	//第一種粒子
 	var particles = [];
+	var particleNums = 70;
 	var particleImage = new Image();
-	particleImage.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/82015/snowflake.png';
+	particleImage.src = 'images/particle_snowflake.png';
 
-	window.onorientationchange = function(){
+	//第一種粒子
+	var particles2 = [];
+	var particleNums2 = 10;
+	var particleImage2 = new Image();
+	particleImage2.src = 'images/particle_snowflake2.png';
+	window.addEventListener('devicemotion',  function(){
+		alert("devicemotion")
 		reStart();
-	}
+	});
+	window.addEventListener('deviceorientation',  function(){
+		alert("deviceorientation")
+		reStart();
+	});
 	document.querySelector('.test-box').addEventListener("click", function() {
 		reStart();
 	});
+
 	init();
+	var interval = setInterval( loop, 1500 / 60 );
+	//anime();
 
 	function init() {
-
 		container = document.getElementById('snow-container');
 		document.body.appendChild(container);
 
@@ -40,11 +54,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 		renderer = new THREE.CanvasRenderer();
 		renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+		//創建物件1
 		var material = new THREE.ParticleBasicMaterial( { map: new THREE.Texture(particleImage) } );
+		for (var i = 0; i < particleNums; i++) {
 
-		for (var i = 0; i < 100; i++) {
-
-			particle = new Particle3D( material);
+			particle = new Particle3D(material);
 			particle.position.x = Math.random() * 2000 - 1000;
 			particle.position.y = Math.random() * 2000 - 1000;
 			particle.position.z = Math.random() * 2000 - 1000;
@@ -54,15 +69,46 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			particles.push(particle);
 		}
 
+		//創建物件2
+		var material2 = new THREE.ParticleBasicMaterial( { map: new THREE.Texture(particleImage2) } );
+		for (var i = 0; i < particleNums2; i++) {
+
+			particle2 = new Particle3D(material2);
+			particle2.position.x = Math.random() * 2000 - 1000;
+			particle2.position.y = Math.random() * 2000 - 1000;
+			particle2.position.z = Math.random() * 2000 - 1000;
+			setInterval(function() {
+				particle2.rotation.x += Math.PI * 0.7;
+				particle2.rotation.y += Math.PI * 0.7;
+			},1000)
+
+			particle2.scale.x = particle2.scale.y =  1;
+			scene.add( particle2 );
+
+			particles2.push(particle2);
+
+		}
+
+		//生成物件
 		container.appendChild( renderer.domElement );
 
 
 		document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 		// document.addEventListener( 'touchstart', onDocumentTouchStart, false );
 		// document.addEventListener( 'touchmove', onDocumentTouchMove, false );
+	}
 
-		setInterval( loop, 3000 / 120 );
+	function anime() {
+		//console.log(particle2)
+		setInterval(function() {
+			particle2.rotation.x += 500;
+			particle2.rotation.y += 500;
+		},1000)
 
+		renderer.render(scene, camera);
+		requestAnimationFrame(function() {
+			anime();
+		});
 	}
 
 	function onDocumentMouseMove( event ) {
@@ -109,34 +155,39 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			}
 		}
 
+		for (var i = 0; i < particles2.length; i++)	{
+
+			var particle2 = particles2[i];
+			particle2.updatePhysics();
+
+			with(particle2.position) {
+				if ( y < -1000) y += 2000;
+				if (x > 1000) x -= 2000;
+				else if (x < -1000) x += 2000;
+				if (z > 1000) z -= 2000;
+				else if (z<-1000) z += 2000;
+			}
+
+		}
+
+
+		//控制滑鼠攝影機轉角
 		camera.position.x += ( mouseX - camera.position.x ) * 0.05;
 		camera.position.y += ( - mouseY - camera.position.y ) * 0.05;
+
 		camera.lookAt(scene.position);
 
 		renderer.render( scene, camera );
+
 	}
 	function reStart() {
-		document.body.removeChild(document.getElementById('snow-container'))
 
+		document.body.removeChild(document.getElementById('snow-container'))
 
 		var node = document.createElement("div");
 		node.setAttribute("id","snow-container");
 		document.querySelector('body').appendChild(node);
 
-		var SCREEN_WIDTH = window.innerWidth;
-		var SCREEN_HEIGHT = window.innerHeight;
-
-		var container;
-
-		var particle;
-		var mouseX = 0;
-		var mouseY = 0;
-		var windowHalfX = window.innerWidth / 2;
-		var windowHalfY = window.innerHeight / 2;
-
-		var particles = [];
-		var particleImage = new Image();
-		particleImage.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/82015/snowflake.png';
 		init();
 	}
 })
